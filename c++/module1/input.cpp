66b3b8044605a1f459eabcf8687798408b154043
Atiar-Rahman <atiar1709@gmail.com>
2025-11-21 09:11:45 +0600

    c++ start

---
+++ c++/module1/input.cpp
@@ -0,0 +1,13 @@
+// ...existing code...
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cout << "Enter a number: "; // added prompt
+    cin >> a;
+    cout << "You entered: " << a << endl;
+    return 0;
+}
+// ...existing code...
